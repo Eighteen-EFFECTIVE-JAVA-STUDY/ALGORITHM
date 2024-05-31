79949397de096033d7f51527c86a6e0b8483ef96
박상준 <qkrtkdwns3410@gmail.com>
2024-05-31 21:36:06 +0900

    chore: 1주차 폴더 생성

---
+++ java/신종신/주차1/Main.java
@@ -0,0 +1,18 @@
+package 신종신.주차1;
+
+/**
+ * packageName    : eddy.주차1
+ * fileName       : Main
+ * author         : ipeac
+ * date           : 24. 5. 31.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 24. 5. 31.        ipeac       최초 생성
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
