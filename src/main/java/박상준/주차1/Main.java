a02f5c320fc673c90b61de9f6b34b18476858e6c
박상준 <qkrtkdwns3410@gmail.com>
2024-05-31 22:03:44 +0900

    chore: 수정

---
+++ src/main/java/박상준/주차1/Main.java
@@ -0,0 +1,18 @@
+package 박상준.주차1;
+
+/**
+ * packageName    : eddy.주차1
+ * fileName       : Main
+ * author         : ipeac
+ * date           : 24. 5. 31.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 24. 5. 31.        ipeac       최초 생성
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
