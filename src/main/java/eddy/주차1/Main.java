ce2badc90eed5113cae0dee81aab40ab364f81b2
Yea-Na <tlsdpsk1992@naver.com>
2024-06-04 07:29:52 +0900

    feat: https://www.acmicpc.net/problem/9996

--- src/main/java/eddy/주차1/Main.java
+++ src/main/java/eddy/주차1/Main.java
@@ -1,5 +1,9 @@
 package eddy.주차1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * packageName    : eddy.주차1
  * fileName       : Main
@@ -13,6 +17,34 @@ package eddy.주차1;
  */
 public class Main {
     public static void main(String[] args) {
+
+        int countNo;
+        String pattern;
+
+        Scanner sc = new Scanner(System.in);
+        countNo = Integer.parseInt(sc.nextLine());
+        pattern = sc.nextLine();
+
+        List<String> fileList = new ArrayList<String>();
+        for (int i = 0; i < countNo; i++) {
+            String temp;
+            temp = sc.nextLine();
+
+            fileList.add(temp);
+            System.out.println(fileList);
+        }
+
+        String[] a = pattern.split("/*");
+
+        for (int i = 0; i < fileList.size(); i++) {
+            if ((fileList.get(i).indexOf((a[0])) == 0) & (fileList.get(i).indexOf((a[1])) == -1)) {
+                System.out.println("DA");
+            } else {
+                System.out.println("NE");
+
+            }
+
+        }
 
     }
 }
