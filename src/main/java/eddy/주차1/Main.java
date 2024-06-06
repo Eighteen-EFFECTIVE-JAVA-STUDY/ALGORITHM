b7ad54e0864c9c7ccc6adda168198ed5fbd66bf0
Yea-Na <tlsdpsk1992@naver.com>
2024-06-07 06:28:58 +0900

    feat: https://www.acmicpc.net/problem/9996

--- src/main/java/eddy/주차1/Main.java
+++ src/main/java/eddy/주차1/Main.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 
 /**
- * packageName    : eddy.주차1
+ * packageName    : eddy.주차1.9996
  * fileName       : Main
  * author         : ipeac
  * date           : 24. 5. 31.
@@ -24,23 +24,25 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         countNo = Integer.parseInt(sc.nextLine());
         pattern = sc.nextLine();
-
         List<String> fileList = new ArrayList<String>();
         for (int i = 0; i < countNo; i++) {
             String temp;
             temp = sc.nextLine();
-
             fileList.add(temp);
-            System.out.println(fileList);
         }
 
-        String[] a = pattern.split("/*");
 
-        for (int i = 0; i < fileList.size(); i++) {
-            if ((fileList.get(i).indexOf((a[0])) == 0) & (fileList.get(i).indexOf((a[1])) == -1)) {
+
+        String[] patterns = pattern.split("\\*");
+
+
+        for (int i = 0; i < countNo; i++) {
+            //
+            if ((fileList.get(i).startsWith(patterns[0])) && (fileList.get(i).endsWith(patterns[1])) && (fileList.get(0).length() >= patterns[0].length() + patterns[1].length())) {
                 System.out.println("DA");
             } else {
                 System.out.println("NE");
+
 
             }
 
