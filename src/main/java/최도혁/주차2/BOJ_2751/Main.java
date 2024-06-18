ff78c13774c53fee923fcf1c66508235ad35c2d2
ehgur <ehgur062300@gmail.com>
2024-06-19 08:34:19 +0900

    refactor: 패키지 구조 변경

--- src/main/java/최도혁/주차2/BOJ_2751/Main.java
+++ src/main/java/최도혁/주차2/BOJ_2751/Main.java
@@ -1,9 +1,9 @@
-package 최도혁.주차2.수정렬하기2;
+package 최도혁.주차2.BOJ_2751;
 
 import java.io.*;
 import java.util.TreeSet;
 
-public class BOJ_2751 {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
