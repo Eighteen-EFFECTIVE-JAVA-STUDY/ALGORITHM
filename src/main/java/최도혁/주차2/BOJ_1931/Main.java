e7fbda77aba4ebeaf018816518c5bfa1660cf7fa
ehgur <ehgur062300@gmail.com>
2024-06-19 08:44:38 +0900

    refactor: 패키지 구조 변경

--- src/main/java/최도혁/주차2/BOJ_1931/Main.java
+++ src/main/java/최도혁/주차2/BOJ_1931/Main.java
@@ -1,4 +1,4 @@
-package 최도혁.주차2;
+package 최도혁.주차2.BOJ_1931;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class BOJ_1931 {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
