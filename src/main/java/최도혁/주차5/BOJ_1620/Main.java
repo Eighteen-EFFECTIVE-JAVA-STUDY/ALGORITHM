0c7ed6a534945fd9e48c9f50f00c481c1aeb510e
ehgur <ehgur062300@gmail.com>
2024-07-04 11:31:09 +0900

    feat: 나는야 포켓몬 마스터가 될거야

---
+++ src/main/java/최도혁/주차5/BOJ_1620/Main.java
@@ -0,0 +1,31 @@
+package 최도혁.주차5.BOJ_1620;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        HashMap<String,String> map = new HashMap<>();
+        for(int n=1; n<=N; n++){
+            String name = br.readLine();
+            map.put(name, String.valueOf(n));
+            map.put(String.valueOf(n),name);
+        }
+
+        for(int m=0; m<M; m++){
+            bw.write(map.get(br.readLine()));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+
+}
