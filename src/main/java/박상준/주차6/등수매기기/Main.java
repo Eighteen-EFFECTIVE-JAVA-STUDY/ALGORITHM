fdae2c6a9986e7d4c5e62617e0adf74feff3723e
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-07-07 19:55:52 +0900

    feat: https://www.acmicpc.net/problem/2012

--- src/main/java/박상준/주차6/등수매기기/Main.java
+++ src/main/java/박상준/주차6/등수매기기/Main.java
@@ -3,6 +3,9 @@ package 박상준.주차6.등수매기기;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * packageName    : 박상준.주차6.등수매기기
@@ -19,6 +22,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
+        long answer = 0;
+        
         int N = Integer.parseInt(br.readLine());
+        
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            arr.add(Integer.parseInt(br.readLine()));
+        }
+        
+        Collections.sort(arr);
+        
+        int rank = 1;
+        
+        for (int expected : arr) {
+            answer += Math.abs(rank - expected);
+            rank++;
+        }
+        
+        System.out.println(answer);
     }
 }
