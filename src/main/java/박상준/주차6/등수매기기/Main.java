6cd7444e6a7f9a4ac90670559bd8869408a693ba
박상준 <qkrtkdwns3410@gmail.com>
2024-07-07 16:17:08 +0900

    feat: https://www.acmicpc.net/problem/2012

---
+++ src/main/java/박상준/주차6/등수매기기/Main.java
@@ -0,0 +1,24 @@
+package 박상준.주차6.등수매기기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * packageName    : 박상준.주차6.등수매기기
+ * fileName       : Main
+ * author         : ipeac
+ * date           : 24. 7. 7.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 24. 7. 7.        ipeac       최초 생성
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+    }
+}
