53d983410e0d6c263894306df07fdc84a6934094
ehgur <ehgur062300@gmail.com>
2024-07-24 21:56:43 +0900

    feat: 문자열 폭발

---
+++ src/main/java/최도혁/주차7/BOJ_9935/Main.java
@@ -0,0 +1,36 @@
+package 최도혁.주차7.BOJ_9935;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static final String FRULA = "FRULA";
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String trigger = br.readLine();
+        int triggerLength = trigger.length();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : str.toCharArray()) {
+            sb.append(c);
+            if (sb.length() >= triggerLength) {
+                boolean match = true;
+                for (int i = 0; i < triggerLength; i++) {
+                    if (sb.charAt(sb.length() - triggerLength + i) != trigger.charAt(i)) {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match) {
+                    sb.delete(sb.length() - triggerLength, sb.length());
+                }
+            }
+        }
+
+        System.out.print(sb.length() == 0 ? FRULA : sb.toString());
+    }
+}
