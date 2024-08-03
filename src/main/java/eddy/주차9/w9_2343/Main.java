284c1490f2a3a49d689fa4f915a3f77ee03f3f7b
Yea-Na <tlsdpsk1992@naver.com>
2024-08-03 19:00:33 +0900

    feat: https://www.acmicpc.net/problem/2343

--- src/main/java/eddy/주차9/w9_2343/Main.java
+++ src/main/java/eddy/주차9/w9_2343/Main.java
@@ -3,7 +3,6 @@ package eddy.주차9.w9_2343;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
