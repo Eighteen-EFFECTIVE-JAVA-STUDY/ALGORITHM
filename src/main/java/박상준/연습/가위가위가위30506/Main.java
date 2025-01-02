e02868103be4fac440911a59e39144de1ea9987a
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2025-01-03 00:08:01 +0900

    feat: hhttps://www.acmicpc.net/problem/9663 > 백트래킹

---
+++ src/main/java/박상준/연습/가위가위가위30506/Main.java
@@ -0,0 +1,18 @@
+package 박상준.연습.가위가위가위30506;
+
+/**
+ * packageName    : 박상준.연습.가위가위가위30506
+ * fileName       : Main
+ * author         : ipeac
+ * date           : 25. 1. 2.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 25. 1. 2.        ipeac       최초 생성
+ */
+public class Main {
+    public static void main(String[] args) {
+    
+    }
+}
