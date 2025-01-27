bf5984fd3146f82a418ec1643e93dfefb03d2967
박상준 <qkrtkdwns3410@gmail.com>
2025-01-28 01:32:29 +0900

    feat: https://www.acmicpc.net/problem/1918

---
+++ src/main/java/박상준/연습/LCS2/Main.java
@@ -0,0 +1,18 @@
+package 박상준.연습.LCS2;
+
+/**
+ * packageName    : 박상준.연습.LCS2
+ * fileName       : Main
+ * author         : Jun
+ * date           : 25. 1. 28.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 25. 1. 28.        Jun       최초 생성
+ */
+public class Main {
+    public static void main(String[] args) {
+    
+    }
+}
