ec5be5794706681254340bca22fa3fe6ed51f2ff
박상준 <sjunpark@itembay.co.kr>
2025-02-05 15:10:34 +0900

    알고리즘

---
+++ src/main/java/박상준/구름에듀/놀이공원/Main.java
@@ -0,0 +1,19 @@
+package 박상준.구름에듀.놀이공원;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		// 공터 크기 N , 구매할 땅의 크기 K
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+	}
+}
---
+++ src/main/java/박상준/연습/좋다1253/Main.java
@@ -0,0 +1,69 @@
+package 박상준.연습.좋다1253;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/*
+3
+0 0 0
+
+8
+* */
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            // N 개의 수 중에 특정 값이 다른 수 2개의 합으로 나타낼 수 있따면 해당 수를 "좋다" 라고 한다.
+            int N = Integer.parseInt(br.readLine());
+            
+            int[] arr = new int[N];
+            
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            
+            //투포인터 정렬 필수
+            Arrays.sort(arr);
+            
+            int answer = 0;
+            
+            // 첫번쨰 숫자부터 순회한다.
+            for (int currentIndex = 0; currentIndex < N; currentIndex++) {
+                int currentNumber = arr[currentIndex];
+                // 본인 인덱스보다 낮은 친구들로만 순회해야함.
+                int leftIndex = 0;
+                int rightIndex = N - 1;
+                
+                while (true) {
+                    if (leftIndex == currentIndex) {
+                        leftIndex++;
+                    } else if (rightIndex == currentIndex) {
+                        rightIndex--;
+                    }
+                    
+                    //불가능한 조건
+                    if (leftIndex >= rightIndex) {
+                        break;
+                    }
+                    
+                    long sum = arr[leftIndex] + arr[rightIndex];
+                    
+                    if (sum == currentNumber) {
+                        answer++;
+                        break;
+                    } else if (sum < currentNumber) {
+                        leftIndex++;
+                    } else {
+                        rightIndex--;
+                    }
+                }
+            }
+            
+            System.out.println(answer);
+        }
+    }
+}
